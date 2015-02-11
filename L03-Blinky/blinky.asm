3516b9bb74362149d9ba5d13c0dcd9a4663e4557
Robert James Williams <rjava@louisiana.cs.byu.edu>
2015-02-11 16:42:23 -0700

    finished

--- L03-Blinky/blinky.asm
+++ L03-Blinky/blinky.asm
@@ -13,15 +13,15 @@
 ;
 ; Show all calculations:
 ;
-;   MCLK = _______ cycles / _______ interval = _______ Mhz
-;    CPI = _______ cycles/ _______ instructions = _______ Cycles/Instruction
-;   MIPS = MCLK / CPI / 1000000 = _______ MIPS
+;   MCLK = 90384 cycles / .1s interval = 1.2 Mhz
+;    CPI = 26 cycles/ 15 instructions = 1.73 Cycles/Instruction
+;   MIPS = MCLK / CPI / 1000000 = .69 MIPS
 
 ;
 ;*******************************************************************************
            .cdecls  C,"msp430.h"            ; MSP430
 
-SMALL_COUNT      	.equ    0x7600			; small subrouting count
+SMALL_COUNT      	.equ    0x75b0			; small subrouting count
 FLASH_COUNT			.equ	0x03			; count of three subloops per flash
 WAIT_COUNT			.equ	0x061			; count of 97 subloops per 10 seconds (plus flash)
 
@@ -39,21 +39,21 @@ toggle_led:
 			xor.w	#0x01,r5				; toggle status (length 1, cycles 1)
 			mov.w	#FLASH_COUNT,r14		; (length 2, cycles 2)
 			cmp.w	#0x0,r5					; (length 1, cycles 1)
-			jne		big_timing_loop			; (length 1, cycles 1)
+			jne		big_timing_loop			; (length 1, cycles 2)
 			mov.w	#WAIT_COUNT,r14			; (length 2, cycles 2)
-			jmp		big_timing_loop			; (length 1, cycles 1)
+			jmp		big_timing_loop			; (length 1, cycles 2)
 
 small_timing_loop:
 			sub.w 	#0x01,r15				; subtract 1 from r15 (length 1, cycles 1)
 			and.w	r15,r15					; waste 1 cycle (length 1, cycles 1)
-			jnz small_timing_loop			; redo loop if r15 != 0 (length 1, cycles 1)
-			jmp big_timing_loop				; (length 1, cycles 1)
+			jnz small_timing_loop			; redo loop if r15 != 0 (length 1, cycles 2)
+			jmp big_timing_loop				; (length 1, cycles 2)
 
 big_timing_loop:
 			sub.w 	#0x01,r14				; subtract 1 from r14 (length 1, cycles 1)
 			mov.w	#SMALL_COUNT,r15		; (length 2, cycles 2)
-			jnz small_timing_loop			; redo loop if r14 != 0 (length 1, cycles 1)
-			jmp toggle_led					; toggle the led (length 1, cycles 1)
+			jnz small_timing_loop			; redo loop if r14 != 0 (length 1, cycles 2)
+			jmp toggle_led					; toggle the led (length 1, cycles 2)
 
 ;------------------------------------------------------------------------------
 ;           Interrupt Vectors
