c2239f3d7b92adfbd2b66561d52e65cb901c9347
Robert James Williams <rjava@ohio.cs.byu.edu>
2015-03-19 17:16:09 -0600

    started morse 2

---
+++ L07-Morse2/Debug/main.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430f2274.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+main.obj: ../morse2.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/ctype.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/_isfuncdcl.h
+
+../main.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430f2274.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+../morse2.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/ctype.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/_isfuncdcl.h: 
