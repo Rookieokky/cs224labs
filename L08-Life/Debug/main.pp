e46397c433c0363443ee1883f088e7a38be6e3d2
Robert James Williams <rjava@illinois.cs.byu.edu>
2015-03-27 11:39:06 -0600

    started life lab

---
+++ L08-Life/Debug/main.pp
@@ -0,0 +1,31 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/string.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430f2274.h
+main.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+main.obj: ../RBX430-1.h
+main.obj: ../RBX430_lcd.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h
+main.obj: ../life.h
+main.obj: ../lifelib.h
+
+../main.c: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/string.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430f2274.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+../RBX430-1.h: 
+../RBX430_lcd.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h: 
+../life.h: 
+../lifelib.h: 
