8e8625bf598db767a7908453513d4461af368adc
Robert James Williams <rjava@michigan.cs.byu.edu>
2015-03-30 22:18:34 -0600

    still trying

--- L08-Life/life.h
+++ L08-Life/life.h
@@ -7,8 +7,7 @@
 #define myCLOCK			1200000			// 1.2 Mhz clock
 #define CLOCK			_1MHZ
 
-#define SET_BIT(row, col) life[(row) >> 2][(col) >> 4] |= ((uint16)0x0001 << (((((row >> 1) << 1) + (col >> 1)) << 2) + ((row % 2) << 1 + (col % 2))))
-#define CLEAR_BIT(row, col) life[(row) >> 2][(col) >> 4] &= ~((uint16)0x0001 << (((((row >> 1) << 1) + (col >> 1)) << 2) + ((row % 2) << 1 + (col % 2))))
-#define TEST_BIT(row, col) life[(row) >> 2][(col) >> 4] & ((uint16)0x0001 << (((((row >> 1) << 1) + (col >> 1)) << 2) + ((row % 2) << 1 + (col % 2))))
+#define SET_BIT(row, col) life[(row)][(col) >> 4] |= (0x0001 << (col & 0x000F))
+#define CLEAR_BIT(row, col) life[(row)][(col) >> 4] &= (0x0001 << (col & 0x000F))
 
 #endif /* LIFE_H_ */
